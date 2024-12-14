8da07339f62d17ccbc3332555f3c50f261550bb5
Daniel-Pincay <86489085+El-Buen-Alex@users.noreply.github.com>
2024-12-14 13:43:20 -0500

    fix: listo para produccion

--- init-db.sh
+++ init-db.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 while true; do
-    /opt/mssql-tools/bin/sqlcmd -S sqlsrv -U sa -P $MSSQL_SA_PASSWORD -Q "SELECT 1" -t 5 > /dev/null 2>&1
+    /opt/mssql-tools/bin/sqlcmd -S sqlsrv -U $MSSQL_USER -P $MSSQL_SA_PASSWORD -Q "SELECT 1" -t 5 > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "SQL Server está disponible"
         echo "IF NOT EXISTS (SELECT * FROM master.sys.databases WHERE name = '$MSSQL_DB_NAME') BEGIN EXEC('CREATE DATABASE $MSSQL_DB_NAME'); END" > /tmp/init-db.sql
-        /opt/mssql-tools/bin/sqlcmd -S sqlsrv -U sa -P $MSSQL_SA_PASSWORD -i /tmp/init-db.sql
+        /opt/mssql-tools/bin/sqlcmd -S sqlsrv -U $MSSQL_USER -P $MSSQL_SA_PASSWORD -i /tmp/init-db.sql
         echo "SCHEMA CREADO"
+        echo "Ejecutando migraciones y seeders de Laravel..."
+        cd /var/www/html
+        php artisan migrate
+        php artisan db:seed
+        echo "Migraciones y seeders ejecutados"
         break
     else
         echo "Esperando por SQL Server..."
